c6c30a1cc19b01d7e9e2c2d76d02b19fd0a8c1fa
hjj183153 <hjj183153@163.com>
2019-07-31 21:06:57 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const indexRouter=require('./routers/index.js');
+//创建web服务器
+var server=express();
+server.listen(3000);
+//托管静态资源到public目录下
+server.use(express.static('public'));
+//使用body-parser中间件将post请求数据解析为对象
+//注意：一定要写在路由的前边
+server.use(bodyParser.urlencoded({
+  extended:false
+}));
+//把首页路由器挂载到/index
+// /index/register
+server.use('/index',indexRouter);
